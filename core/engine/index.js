ace58f4682533527cbc1b7fdb2d7f077e352e795
Luan Gabriel Mantuani <1811420257@fema.edu.br>
2021-05-15 16:59:37 -0300

    Lista de palavras reservadas

--- core/engine/index.js
+++ core/engine/index.js
@@ -1,20 +1,226 @@
-var palavrasReservadas = [{
+var palavrasReservadas = [
+  {
     token: 'vol',
     codigoJs: 'var'
-}]; //todas as palavras reservadas
-var tiposVariaveis = [{
+  },
+  {
     token: '@int',
-    tipoJs: 'number'
-}]; // todos os tipos de variaveis
-var simbolosReservados = [{
-    token: '`',
-    codigoJs: '{'
-}]; //todos os simbolos da linguagem
-var operadoresLogicos = [{
-    token: 'equals',
-    codigoJs: '=='
-}];
-var operadoresAritmeticos = [{
+    codigoJs: 'number'
+  },
+  {
+    token: '@flt',
+    codigoJs: 'number'
+  },
+  {
+    token: '@str',
+    codigoJs: 'string'
+  },
+  {
+    token: '@bool',
+    codigoJs: 'boolean'
+  },
+  {
+    token: 'is',
+    codigoJs: '='
+  },
+  {
+    token: 'input',
+    codigoJs: 'INPUT'
+  },
+  {
+    token: 'output',
+    codigoJs: 'OUTPUT'
+  },
+  {
+    token: 'condition',
+    codigoJs: 'if'
+  },
+  {
+    token: 'otherwise',
+    codigoJs: 'else'
+  },
+  {
+    token: 'loop',
+    codigoJs: 'while'
+  },
+  {
     token: 'plus',
     codigoJs: '+'
-}];
+  },
+  {
+    token: 'minus',
+    codigoJs: '-'
+  },
+  {
+    token: 'times',
+    codigoJs: '*'
+  },
+  {
+    token: 'by',
+    codigoJs: '/'
+  },
+  {
+    token: 'remainder',
+    codigoJs: '%'
+  },
+  {
+    token: 'pot',
+    codigoJs: 'POTÊNCIA'
+  },
+  {
+    token: 'root',
+    codigoJs: 'RAIZ'
+  },
+  {
+    token: 'greaterthan',
+    codigoJs: '>'
+  },
+  {
+    token: 'lessthan',
+    codigoJs: '<'
+  },
+  {
+    token: 'equals',
+    codigoJs: '=='
+  },
+  {
+    token: 'notequals',
+    codigoJs: '!='
+  },
+  {
+    token: 'and',
+    codigoJs: '&&'
+  },
+  {
+    token: 'or',
+    codigoJs: '||'
+  },
+  {
+    token: 'not',
+    codigoJs: '!'
+  },
+  {
+    token: 'true',
+    codigoJs: 'true'
+  },
+  {
+    token: 'false',
+    codigoJs: 'false'
+  },
+]; //todas as palavras reservadas
+var tiposVariaveis = [
+  {
+    token: '@int',
+    tipoJs: 'number'
+  },
+  {
+    token: '@flt',
+    tipoJs: 'number'
+  },
+  {
+    token: '@str',
+    tipoJs: 'string'
+  },
+  {
+    token: '@bool',
+    tipoJs: 'boolean'
+  },
+]; // todos os tipos de variaveis
+var simbolosReservados = [
+  {
+    token: '<',
+    codigoJs: '('
+  },
+  {
+    token: '>',
+    codigoJs: ')'
+  },
+  {
+    token: ';',
+    codigoJs: ';'
+  },
+  {
+    token: '`',
+    codigoJs: '{'
+  },
+  {
+    token: '´',
+    codigoJs: '}'
+  },
+  {
+    token: '"',
+    codigoJs: '"'
+  },
+  {
+    token: '|',
+    codigoJs: '+'
+  },
+  {
+    token: '#',
+    codigoJs: '//'
+  },
+  {
+    token: '.',
+    codigoJs: '.'
+  },
+]; //todos os simbolos da linguagem
+var operadoresLogicos = [
+  {
+    token: 'greaterthan',
+    codigoJs: '>'
+  },
+  {
+    token: 'lessthan',
+    codigoJs: '<'
+  },
+  {
+    token: 'equals',
+    codigoJs: '=='
+  },
+  {
+    token: 'notequals',
+    codigoJs: '!='
+  },
+  {
+    token: 'and',
+    codigoJs: '&&'
+  },
+  {
+    token: 'or',
+    codigoJs: '||'
+  },
+  {
+    token: 'not',
+    codigoJs: '!'
+  },
+];
+var operadoresAritmeticos = [
+  {
+    token: 'plus',
+    codigoJs: '+'
+  },
+  {
+    token: 'minus',
+    codigoJs: '-'
+  },
+  {
+    token: 'times',
+    codigoJs: '*'
+  },
+  {
+    token: 'by',
+    codigoJs: '/'
+  },
+  {
+    token: 'remainder',
+    codigoJs: '%'
+  },
+  {
+    token: 'pot',
+    codigoJs: 'POTÊNCIA'
+  },
+  {
+    token: 'root',
+    codigoJs: 'RAIZ'
+  },
+];
