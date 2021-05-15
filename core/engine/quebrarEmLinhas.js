4356c9a853f632e615a225e65d019cb75f4981e8
Jacques <jacquesvst@gmail.com>
2021-05-15 17:35:52 -0300

    melhoras na regex

--- core/engine/quebrarEmLinhas.js
+++ core/engine/quebrarEmLinhas.js
@@ -2,9 +2,11 @@ import { Linha } from './models/linha.js';
 
 export function quebrarCodigoEmArrayPorLinha(codigo) {
     let arrayDeLinhas = [];
-    let separadoPorFimDeLinha = codigo.split(/;|`|´/);
-    let removidoEspaco = separadoPorFimDeLinha.map(s => s.trim());
+
+    let codigoSeparadoPorLinhas = codigo.split(/(?<=;)|(?<=`)|(?<=´)/g);
+    let removidoEspaco = codigoSeparadoPorLinhas.map(s => s.trim());
     let removidoLinhasVazias = removidoEspaco.filter(s => s != '');
+    
     removidoLinhasVazias.forEach((l, i) => {
         let linha = new Linha(l, i, []);
         arrayDeLinhas.push(linha);
