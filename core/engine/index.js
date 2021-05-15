402ac121b737c31e6aeee2f89cc28daddbc40736
Luis Henrique B. Franco <luishbfranco@gmail.com>
2021-05-15 16:08:28 -0300

    objetos com tipos de dados e estruturas predefinidos na linguagem

---
+++ core/engine/index.js
@@ -0,0 +1,20 @@
+var palavrasReservadas = [{
+    token: 'vol',
+    codigoJs: 'var'
+}]; //todas as palavras reservadas
+var tiposVariaveis = [{
+    token: '@int',
+    tipoJs: 'number'
+}]; // todos os tipos de variaveis
+var simbolosReservados = [{
+    token: '`',
+    codigoJs: '{'
+}]; //todos os simbolos da linguagem
+var operadoresLogicos = [{
+    token: 'equals',
+    codigoJs: '=='
+}];
+var operadoresAritmeticos = [{
+    token: 'plus',
+    codigoJs: '+'
+}];
