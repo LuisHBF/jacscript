9ce110933b1e4d974fdc91d6ce33b7859a07c365
Luan Mantuani <luangmantuani@gmail.com>
2021-06-09 23:29:14 -0300

    Definindo tema escuro no textarea

--- core/interface/index.js
+++ core/interface/index.js
@@ -2,7 +2,6 @@ let codeMirror = new CodeMirror.fromTextArea(
   document.getElementById("editor"),
   { lineNumbers: true, mode: "javascript", theme: "default" }
 );
-
 codeMirror.setSize("100%", "100%")
-
+codeMirror.refresh();
 export default codeMirror;
